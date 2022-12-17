ac8db9eb6e0de2540abfe06faf7cb7d586ec48dd
251 <2733064591@qq.com>
2022-12-17 13:04:29 +0800

    Site updated: 2022-12-17 13:04:29

---
+++ js/Valine.js
@@ -0,0 +1,16 @@
+
+new Valine({
+    el: '#vcomments',
+    appId: 'QINGPhF2VsDTnwPMlq85SYYQ-gzGzoHsz',
+    appKey: 'Mzs7Yd6J5PLg687lQdqkvafS'
+})
+
+// Use import
+import Valine from 'valine';
+// or Use require
+const Valine = require('valine');
+
+// new Valine({
+//     el:'#vcomments',
+//     // other config
+// })
